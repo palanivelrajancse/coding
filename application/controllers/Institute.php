6f3df797549a5325b77c4f580f8a951b6d5779c4
palanivelrajancse <palanivelrajancse@gmail.com>
2018-11-13 18:15:41 +0530

    changes done

--- application/controllers/Institute.php
+++ application/controllers/Institute.php
@@ -8,11 +8,19 @@ $this->load->view('Institute_selection');
 $checked = $this->input->post('course');
 if(!empty($checked))
 	{
-		
+
 	if(in_array('iitm',$checked) && in_array('iitd1',$checked) && in_array('iitd2',$checked) && in_array('ism',$checked))
 		{
 		echo "IITM & IITD & ISM";
-		}		
+		}
+		elseif(in_array('iitm',$checked) && in_array('iitd1',$checked) && in_array('ism',$checked))
+		{
+			echo "IITM & IITD & ISM";
+		}
+		elseif(in_array('iitm',$checked) && in_array('iitd2',$checked) && in_array('ism',$checked))
+		{
+			echo "IITM & IITD & ISM";
+		}
 		elseif(in_array('iitm',$checked) && in_array('iitd1',$checked) && in_array('iitd2',$checked))
 		{
 		echo "IITM & IITD";
@@ -36,16 +44,16 @@ if(!empty($checked))
 				elseif(in_array('ism',$checked) && in_array('iitd1',$checked))
 				{
 				echo "IITD & ISM";
-				}				
+				}
 				elseif(in_array('ism',$checked) && in_array('iitd2',$checked))
 				{
 				echo "IITD & ISM";
 				}
-						
+
 						elseif(in_array('iitm',$checked))
 						{
 						echo "IITM";
-						}	
+						}
 						elseif(in_array('ism',$checked))
 						{
 						echo "ISM";
@@ -58,6 +66,7 @@ if(!empty($checked))
 						{
 						echo "IITD";
 						}
+
 	}
 }
 
